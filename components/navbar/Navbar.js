d8b1bb054b9c0095fb5eb32e59d9fbdca5e55c29
CASE2  - ilman teguh prasetya <ilmanfrasetya@gmail.com>
2022-04-22 17:31:42 +0700

    good

---
+++ components/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () =>{
+    return(
+        <div className='navbar'>
+            <h3 className='text-pink-300 text-sm'>Navbar ganteng</h3>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ container/home/HomeLayout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+
+
+const HomeLayout = () =>{
+    return(
+        <div className='home'>
+            <Navbar/>
+            <p>Ilman teguh prasetya</p>
+        </div>
+    )
+}
+
+export default HomeLayout;
